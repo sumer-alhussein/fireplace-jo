d92d69560e3200bb5b0a2245952ce3c0cdda7d55
sumer-alhussein <somar.alhussien@gmail.com>
2022-12-26 21:02:35 +0300

    on work

---
+++ src/screens/main/mainContainer.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class MainContainer extends Component {
+  render() {
+    return (
+      <View>
+        <Text>mainContainer</Text>
+      </View>
+    )
+  }
+}
+
+export default MainContainer
